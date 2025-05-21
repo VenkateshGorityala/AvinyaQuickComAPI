b0bb65433991db6c821e2817a4b883c53d2c09f2
VenkateshGorityala <gorityalavenkatesh9100@gmail.com>
2025-05-21 12:09:14 +0530

    adding new controller

---
+++ VenkateshQuickCommerceAPI/Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VenkateshQuickCommerceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet("GetAllCategory")]
+        public void GetAllCategory()
+        { 
+        
+        }
+    }
+}
